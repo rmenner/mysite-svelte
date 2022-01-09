79c4d38796150a15fd76febdbbb0d4d52b774b07
Ryan Menner <rwmenner@gmail.com>
2022-01-09 12:26:19 -0500

    edit: copy changes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,10 +4,10 @@ import Page from '../components/page.svelte'
 
   const metadata = {
     page: {
-      title: 'Greetings'
+      title: 'Design. Prototype. Build.'
     },
-		title: 'About',
-		description: 'This my site',
+		title: 'Design. Prototype. Build.',
+		description: 'My mission, to create work that is both useful and beautiful.',
 		image: 'https://svelte.dev/images/twitter-card.png',
 	}
 
@@ -16,12 +16,17 @@ import Page from '../components/page.svelte'
 
 <Page {metadata}>
 
-  <div class="space-y-6 text-xl font-light text-gray-500 leading-relaxed tracking-wide">
-    <p>
-      My name is Ryan and I live in famously hot Columbia, South Carolina. Creating great design, eating pepperoni pizza, flying rc aircraft, and living in faith makes up my recipe for a happy and fulfilled life.
-    </p>
-    <p>
+  <div class="space-y-6 text-lg font-light text-gray-500 leading-relaxed tracking-wide">
+
+    <p class="text-2xl">
       As a designer, developer, and individual I strive to do everything with diligence, integrity, and fervor. Always looking for a way to create work that is both useful and beautiful.
+    </p>
+
+    <div></div>
+
+    <h2 class="text-3xl pt-20 font-light text-teal-700">Greetings</h2>
+    <p>
+      My name is Ryan and I live in famously hot Columbia, South Carolina with my wife, two dogs, chickens, and fish. Creating great design, eating pepperoni pizza, flying rc aircraft, and living in faith makes up my recipe for a happy and fulfilled life.
     </p>
 
     <div class="flex items-center justify-end pr-8 pt-12">
