dd64e24cfaeadb9a326273eefc33989d3a364c4f
Ryan Menner <rwmenner@gmail.com>
2022-01-09 15:17:12 -0500

    edit: copy changes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -25,9 +25,8 @@ import Page from '../components/page.svelte'
     <div></div>
 
     <h2 class="text-3xl pt-20 font-light text-teal-700">Greetings</h2>
-    <p>
-      My name is Ryan and I live in famously hot Columbia, South Carolina with my wife, two dogs, chickens, and fish. Creating great design, eating pepperoni pizza, flying rc aircraft, and living in faith makes up my recipe for a happy and fulfilled life.
-    </p>
+    <p>My name is Ryan and I live in famously hot Columbia, South Carolina with my wife, dogs, chickens, and fish. Outside of web development, I enjoy 3D printing and building/flying FPV quadcopters.</p>
+    <p> I have been designing responsive web experiences for over 10 years. Specializing in simple mobile first user experiences with an emphasis on performance. I love to learn new things and enjoy teaching others. If you need my help for a project or would like some coaching/mentorship, please feel free to reach out.</p>
 
     <div class="flex items-center justify-end pr-8 pt-12">
       <div class="mr-3 text-right leading-snug">
--- src/routes/resume.svelte
+++ src/routes/resume.svelte
@@ -23,6 +23,10 @@
         <h3 class="uppercase text-teal-600 tracking-widest">Overview</h3>
       </div>
 
+      <div class="prose">
+        <p>I do not have what you would call a traditional education. I am one of those "Homeschooled" kids. Learning how to learn was the first skill I acquired in that kind of educational environment. Continue that mindset 20 years, I have been able to gain a wealth of experience.</p>
+      </div>
+
       <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 gap-8 pb-8">
         {#each overview as { title, list }, i}
           <div class="shadow p-5">
@@ -71,28 +75,6 @@
           </li>
         {/each}
       </ul>
-      
-    </div>
-
-    <div class="space-y-6 font-light text-gray-500 leading-relaxed tracking-wide pt-14">
-      
-      <div class="border-t-2 border-teal-600 pt-5 mt-5 pb-5">
-        <h3 class="uppercase text-teal-600 tracking-widest">Education</h3>
-      </div>
-      <div class="prose">
-        <p>I do not have what you would call a traditional education. My education started in the home. Simply put I am one of those "Home Schooled" kids. Learning how to learn was the first skill I acquired in that kind of educational environment.</p>
-        <p>Fast forward that mindset 20 years coupled with job opportunities that allowed me to gain a wealth of experience. That being said I know enough to see just how much I don't know. I don't claim be an expert coder but I am willing to learn and am really hoping that one day I will find myself in a place where I can build anything I put my mind to.</p>
-        <p>Here is a list of sources I have used to supplement my experience based learning.</p>
-        <ul>
-          <li>freecodecamp.com</li>
-          <li>projecteuler.net</li>
-          <li>lynda.com</li>
-          <li>teamtreehouse.com</li>
-          <li>codepen.io</li>
-          <li>vtc.com</li>
-        </ul>
-        <p>Aside from my unconventional education I have also participated in some honest to goodness traditional education. Through my junior and senior high school days I had the opportunity to take some classes part-time for some of the subjects we didn't have the resources or know-how to learn at home. Those classes include art, computer art, algebra, and other electives like the school newspaper. Also I attended a few classes at the local community college focusing my attention on math, literature, art, and web technologies.</p>
-      </div>
       
     </div>
 
